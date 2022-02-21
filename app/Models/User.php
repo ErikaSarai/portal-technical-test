5525d626e97e23a56c03191b56d6974709cf1884
ErikaSarai <lindanatera2013@gmail.com>
2022-02-21 18:29:41 -0400

    adding ui and main settings

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number_code',
+        'phone_number',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone_number_code');
+            $table->integer('phone_number');
             $table->rememberToken();
             $table->timestamps();
         });
